b6f8b287b766eb48896422df79087d15bf21d996
Elias Peteri <elias.peteri@hotmail.com>
2021-02-01 15:02:34 +0200

    created app and moved project files one folder up

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Navbar from "./Navbar";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+        </>
+    );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">About me</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Contact me</a>
+        </nav>
+    );
+};
+
+export default Navbar;
