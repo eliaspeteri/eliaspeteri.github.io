c8b2f44e7f5f7f9dfea82830bf61dae873d0e884
Elias Peteri <elias.peteri@hotmail.com>
2021-05-19 20:11:20 +0300

    fixed Footer export error

--- src/components/index.js
+++ src/components/index.js
@@ -6,3 +6,4 @@ export { default as About } from "./About";
 export { default as Portfolio } from "./Portfolio";
 export { default as Home } from "./Home/Home";
 export { default as Resume } from "./Resume/Resume";
+export { default as Footer } from "./Footer";
